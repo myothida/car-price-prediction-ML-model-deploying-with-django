99e4a424f05962a67b45423c6054688a1b40cfc9
anshuman <anshumangupta5050@gmail.com>
2020-07-28 09:12:10 +0530

    added

--- price_predictor/settings.py
+++ price_predictor/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heruko
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heruko.settings(locals())
 
 
